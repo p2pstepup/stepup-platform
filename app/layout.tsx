c255152102be2f3b3a8d480eeb5d711e0cf1d4a4
Tai Donovan <taidonovan@MacBook-Air.local>
2026-04-20 21:06:59 -0500

    Set global font size to 110%

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{fontSize: '110%'}}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600&display=swap" rel="stylesheet"/>
       </head>
-      <body style={{margin: 0, padding: 0, zoom: '110%'}}>{children}</body>
+      <body style={{margin: 0, padding: 0}}>{children}</body>
     </html>
-  );
+  )
 }
